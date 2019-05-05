2b6f06021d1db6383cc6eba77a8cdfd9a66d3e25
cclauss <cclauss@me.com>
2019-05-05 11:43:57 +0200

    Update DominoBoard.py

--- DominoBoard.py
+++ DominoBoard.py
@@ -46,7 +46,6 @@ class DominoBoard(tkDominoBoard):
         number_list = []
         for d in self.mPlayedDominos:
             number_list + d.playable_numbers
-        assert number_list, number_list  # FIXME
         return sorted(set(number_list))
 
     def playableDominos(self, inDomino):
